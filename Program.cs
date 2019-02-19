83c35c318b808c804cc0f352a2bf093cad09b79a
nitr0n <michael.nikolaus@gmail.com>
2019-02-19 18:49:03 +0100

    Initial Console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Indicators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
